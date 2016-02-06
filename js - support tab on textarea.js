e774e636d8f42fd3c1516564c76f92e24cfd367c
Leandro Fernandes <leandromoh@hotmail.com>
2016-02-06 14:24:05 -0200

    faltava algumas funções

--- js - support tab on textarea.js
+++ js - support tab on textarea.js
@@ -1,37 +1,65 @@
-    // font: http://jsfiddle.net/joaocolombo/TQYmB/6/
+<textarea id="asd" cols="50" rows="10" ></textarea>
 
-    var textarea = document.getElementsByTagName('textarea')[0]; 
+<script>
+	// font: http://jsfiddle.net/joaocolombo/TQYmB/3/
+
+	HTMLTextAreaElement.prototype.getCaretPosition = function() {
+		return this.selectionStart;
+	};
+	HTMLTextAreaElement.prototype.setCaretPosition = function(position) {
+		this.selectionStart = position;
+		this.selectionEnd = position;
+		this.focus();
+	};
+	HTMLTextAreaElement.prototype.hasSelection = function() {
+		if (this.selectionStart == this.selectionEnd) {
+			return false;
+		} else {
+			return true;
+		}
+	};
+	HTMLTextAreaElement.prototype.getSelectedText = function() {
+		return this.value.substring(this.selectionStart, this.selectionEnd);
+	};
+	HTMLTextAreaElement.prototype.setSelection = function(start, end) {
+		this.selectionStart = start;
+		this.selectionEnd = end;
+		this.focus();
+	};
     
-    textarea.onkeydown = function(event) {
+    document.getElementById("asd").onkeydown = function(event) {
         
-        //support tab on textarea
+        //support tab on this
         if (event.keyCode == 9) { //tab was pressed
             var newCaretPosition;
-            newCaretPosition = textarea.getCaretPosition() + "    ".length;
-            textarea.value = textarea.value.substring(0, textarea.getCaretPosition()) + "    " + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
-            textarea.setCaretPosition(newCaretPosition);
+            newCaretPosition = this.getCaretPosition() + "    ".length;
+            this.value = this.value.substring(0, this.getCaretPosition()) + "    " + this.value.substring(this.getCaretPosition(), this.value.length);
+            this.setCaretPosition(newCaretPosition);
             return false;
         }
         if(event.keyCode == 8){ //backspace
-            if (textarea.value.substring(textarea.getCaretPosition() - 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
+            if (this.value.substring(this.getCaretPosition() - 4, this.getCaretPosition()) == "    ") { //it's a tab space
                 var newCaretPosition;
-                newCaretPosition = textarea.getCaretPosition() - 3;
-                textarea.value = textarea.value.substring(0, textarea.getCaretPosition() - 3) + textarea.value.substring(textarea.getCaretPosition(), textarea.value.length);
-                textarea.setCaretPosition(newCaretPosition);
+                newCaretPosition = this.getCaretPosition() - 3;
+                this.value = this.value.substring(0, this.getCaretPosition() - 3) + this.value.substring(this.getCaretPosition(), this.value.length);
+                this.setCaretPosition(newCaretPosition);
             }
         }
         if(event.keyCode == 37){ //left arrow
             var newCaretPosition;
-            if (textarea.value.substring(textarea.getCaretPosition() - 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
-                newCaretPosition = textarea.getCaretPosition() - 3;
-                textarea.setCaretPosition(newCaretPosition);
+            if (this.value.substring(this.getCaretPosition() - 4, this.getCaretPosition()) == "    ") { //it's a tab space
+                newCaretPosition = this.getCaretPosition() - 3;
+                this.setCaretPosition(newCaretPosition);
             }    
         }
         if(event.keyCode == 39){ //right arrow
             var newCaretPosition;
-            if (textarea.value.substring(textarea.getCaretPosition() + 4, textarea.getCaretPosition()) == "    ") { //it's a tab space
-                newCaretPosition = textarea.getCaretPosition() + 3;
-                textarea.setCaretPosition(newCaretPosition);
+            if (this.value.substring(this.getCaretPosition() + 4, this.getCaretPosition()) == "    ") { //it's a tab space
+                newCaretPosition = this.getCaretPosition() + 3;
+                this.setCaretPosition(newCaretPosition);
             }
         } 
     }
+
+
+</script>
