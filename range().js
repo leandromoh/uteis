1cca57e847ab93d14ec135fa008eb787d9b9e250
Leandro Fernandes <leandromoh@hotmail.com>
2016-03-15 10:13:17 -0300

    range() 100% imperative

--- range().js
+++ range().js
@@ -4,26 +4,30 @@ range(0, 30, 5); // => [0, 5, 10, 15, 20, 25]
 range(0, -10, -1); //  => [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
 range(0); // => []
 
-function range(start, count, step) {
+function range(start, limit, step) {
     if(arguments.length == 1) {
-        count = start;
+        limit = start;
         start = 0;
+        step = 1;
     }
-    if(arguments.length < 3) {
-       if (arguments.length == 1) 
-          step = 1;
-       else
-          step = (start < limit) ? 1 : -1;
+    if(arguments.length == 2) {
+        step = (start < limit) ? 1 : -1;
     }
 
-    var func = (start < count) ? function(a, b) { return a < b; }
-                               : function(a, b) { return a > b; }
     var foo = [];
 
-    while(func(start, count)) {
-        foo.push(start);
-        start += step;
+    if(start < limit){
+        while(start < limit) {
+            foo.push(start);
+            start += step;
+        }    
     }
-
+    else{
+        while(start > limit) {
+            foo.push(start);
+            start += step;
+        }    
+    }
+        
     return foo;
 }
