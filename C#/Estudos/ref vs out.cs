cc117306b8b5b99dd1a04eba8a70e53066a5ca7b
Leandro Fernandes <leandromoh@hotmail.com>
2016-05-11 16:43:18 -0300

    Update ref vs out.cs

--- C#/Estudos/ref vs out.cs
+++ C#/Estudos/ref vs out.cs
@@ -2,8 +2,8 @@
 A diferença entre os dois tipos de parâmetros são bem sutis. 
 
 out = não precisamos inicializar a variável que vamos passar
-ref = onde somos obrigados a instancializá-la antes de passar o parâmetro
+ref = somos obrigados a inicializar-la antes de passar
 
-Ambas fazem o mesmo papel, mas de acordo com as boas práticas de programação, 
+Ambas fazem o mesmo papel (passar um parâmetro por referência), mas de acordo com as boas práticas de programação, 
 o recomendado é a utilização do parâmetro por referência do tipo out
 */
