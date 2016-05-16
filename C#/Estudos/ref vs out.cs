600bb7d182d90d22bcda44225a01181a41a63cdf
Leandro Fernandes <leandromoh@hotmail.com>
2016-05-16 20:23:50 -0300

    adicionado exemplo

--- C#/Estudos/ref vs out.cs
+++ C#/Estudos/ref vs out.cs
@@ -1,9 +1,36 @@
-/*
-A diferença entre os dois tipos de parâmetros são bem sutis. 
+using System;
 
-out = não precisamos inicializar a variável que vamos passar
-ref = somos obrigados a inicializar-la antes de passar
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /*
+            Ambas fazem o mesmo papel (passar um parâmetro por referência), mas a diferença entre os dois tipos de parâmetros são bem sutis. 
 
-Ambas fazem o mesmo papel (passar um parâmetro por referência), mas de acordo com as boas práticas de programação, 
-o recomendado é a utilização do parâmetro por referência do tipo out
-*/
+            out = não precisamos inicializar a variável que vamos passar, mas o metodo é obrigado a atribuir um valor ao parâmetro.
+            ref = somos obrigados a inicializar a variável antes de passar, mas o metodo não é obrigado a atribuir um valor ao parâmetro.
+        */
+
+        static void Main()
+        {
+            int x, y;
+
+            y = 5;
+
+            Out(out x);
+            Ref(ref y); // if y was not assigned then you get the error: Use of unassigned local variable 'y'
+
+            Console.Write(x + y); // 8
+            Console.Read();
+        }
+
+        static void Out(out int a)
+        {
+            a = 3; // without this line you get the error: The out parameter 'a' must be assigned to before control leaves the current method
+        }
+
+        static void Ref(ref int a)
+        {
+        }
+    }
+}
