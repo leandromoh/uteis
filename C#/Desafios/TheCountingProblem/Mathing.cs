572bd068502d96b2b5cd352b70c8df4e60e1c79b
Leandro Fernandes <leandromoh@hotmail.com>
2016-07-02 16:48:52 -0300

    Update Mathing.cs

--- C#/Desafios/TheCountingProblem/Mathing.cs
+++ C#/Desafios/TheCountingProblem/Mathing.cs
@@ -8,101 +8,28 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int boolx = 0;
-            bool asd = true;
-            int min = 0, max = 10000;
-            //Console.WriteLine(getZeros(1000000));
+            IEnumerable<int> r1, r2;
+            int min = 0, max = 10 * 1000;
+
             for (; max <= 110020000; max += 10000)
             {
-                if (!asd)
+                Console.WriteLine(max);
+
+                r1 = countDigitsLineRapido(min, max);
+
+                r2 = countDigitsMathing(min, max);
+
+                if (!r1.SequenceEqual(r2))
                 {
-                    boolx++;
+                    printArray(r1);
+                    printArray(r2);
+                    break;
                 }
-
-                if (boolx > 3)
-                {
-                    max -= 30000;
-                }
-
-                #region [a]
-                Console.Write(max + "  ");
-                var temp1 = countDigitsLineRapido(min, max).Take(1);
-                printArray(temp1);
-                Console.Write("   ");
-                #endregion [a]
-                    #region [bla]
-                    var dezmil = 1000;
-                    var r = countDigitsLineRapido(0, dezmil);
-
-                    var x = max - min;
-
-                    int a = 1;
-                    int num;
-                    int b = max / dezmil;
-
-                    if (x >= dezmil)
-                    {
-                        x /= dezmil;
-
-                        for (int f = 0; f < 10; f++)
-                            r[f] *= x;
-
-                        for (; a < b; a++)
-                        {
-                            num = a;
-
-                            while (num != 0)
-                            {
-                                r[num % 10] += dezmil;
-                                num /= 10;
-                            }
-                        }
-                    }
-
-                    num = max / dezmil;
-                    while (num != 0)
-                    {
-                        r[num % 10] += 1;
-                        num /= 10;
-                    }
-                    r[1] -= x;
-
-                    if (x < 11)
-                    {
-                        r[0] = (x - 1) * 300 + 192;
-                        r[0]++;
-                    }
-                    else
-                    {
-                        r[0] = (10 - 1) * 300 + 192;
-                    #endregion [bla]
-                        a = 10;
-                        while (a < b)
-                        {
-                            a += 10;
-                            ;
-                            r[0] += 4000;
-                            r[0] += getZeros(a - 10) * 10000;
-                        }
-
-                        r[0] += getZeros(b) + 1;
-                    }
-
-                    var temp2 = r.Take(1);
-                    printArray(temp2);
-                    int gg = temp1.First();
-
-                    Console.WriteLine("   " + (asd = temp2.First() == gg));
-
-
             }
-  
 
-        end:
             Console.Read();
-
         }
 
         private static int getZeros(int num)
@@ -116,28 +43,72 @@ namespace ConsoleApplication1
                     x++;
                 }
                 num /= 10;
-                
+
             }
 
             return x;
         }
 
-        static bool So1numero1(int num)
+        static int[] countDigitsMathing(int min, int max)
         {
-            while (num != 0)
-            {
-                if (num % 10 != 0)
-                {
-                    return num == 1;
-                }
+            var dezmil = 1000;
+            var r = countDigitsLineRapido(0, dezmil);
 
-                num /= 10;
+            var x = max - min;
+
+            int a = 1;
+            int num;
+            int b = max / dezmil;
+
+            if (x >= dezmil)
+            {
+                x /= dezmil;
+
+                for (int f = 0; f < 10; f++)
+                    r[f] *= x;
+
+                for (; a < b; a++)
+                {
+                    num = a;
+
+                    while (num != 0)
+                    {
+                        r[num % 10] += dezmil;
+                        num /= 10;
+                    }
+                }
             }
 
-            return false;
+            num = max / dezmil;
+            while (num != 0)
+            {
+                r[num % 10] += 1;
+                num /= 10;
+            }
+            r[1] -= x;
+
+            if (x < 11)
+            {
+                r[0] = (x - 1) * 300 + 192;
+                r[0]++;
+            }
+            else
+            {
+                r[0] = (10 - 1) * 300 + 192;
+                a = 10;
+                while (a < b)
+                {
+                    a += 10;
+                    ;
+                    r[0] += 4000;
+                    r[0] += getZeros(a - 10) * 10000;
+                }
+
+                r[0] += getZeros(b) + 1;
+            }
+
+            return r;
         }
-
-
 
         static int[] countDigitsLineRapido(int a, int b)
         {
@@ -164,6 +135,7 @@ namespace ConsoleApplication1
             for (int i = 0, len = c.Count(); i < len; i++)
                 Console.Write(c.ElementAt(i) + " ");
 
+            Console.WriteLine();
         }
 
         static void printTimeSpent(Action action)
@@ -173,14 +145,6 @@ namespace ConsoleApplication1
             stopwatch.Stop();
             GC.Collect();
             Console.WriteLine(stopwatch.Elapsed);
-        }
-    }
-
-    public static class MyExtensions
-    {
-        public static bool isBetween(this int i, int fromInclusive, int toInclusive)
-        {
-            return fromInclusive <= i && i <= toInclusive;
         }
     }
 }
