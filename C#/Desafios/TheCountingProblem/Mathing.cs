3785688dfcc415eea1f23938f09d9b371b41222c
Leandro Fernandes <leandromoh@hotmail.com>
2016-07-02 15:02:36 -0300

    almost done

--- C#/Desafios/TheCountingProblem/Mathing.cs
+++ C#/Desafios/TheCountingProblem/Mathing.cs
@@ -10,55 +10,134 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int min = 0, max = 16500000;
-
-            printTimeSpent(() => printArray(countDigitsLineRapido(min, max)));
-
-            Console.WriteLine();
-            
-            printTimeSpent(() =>
+            int boolx = 0;
+            bool asd = true;
+            int min = 0, max = 10000;
+            //Console.WriteLine(getZeros(1000000));
+            for (; max <= 10020000; max += 10000)
             {
-                var dezmil = 1000;
-                var r = countDigitsLineRapido(0, dezmil);
-
-                var x = max - min;
-
-                int a = 1;
-                int num;
-                int b = max / dezmil;
-
-                if (x >= dezmil)
+                if (!asd)
                 {
-                    x /= dezmil;
+                    boolx++;
+                }
 
-                    for (int f = 0; f < 10; f++)
-                        r[f] *= x;
+                if (boolx > 3)
+                {
+                    max -= 30000;
+                }
 
-                    for (; a < b; a++)
+                #region [a]
+                Console.Write(max + "  ");
+                var temp1 = countDigitsLineRapido(min, max).Take(1);
+                printArray(temp1);
+                Console.Write("   ");
+                #endregion [a]
+                    #region [bla]
+                    var dezmil = 1000;
+                    var r = countDigitsLineRapido(0, dezmil);
+
+                    var x = max - min;
+
+                    int a = 1;
+                    int num;
+                    int b = max / dezmil;
+
+                    if (x >= dezmil)
                     {
-                        num = a;
+                        x /= dezmil;
 
-                        while (num != 0)
+                        for (int f = 0; f < 10; f++)
+                            r[f] *= x;
+
+                        for (; a < b; a++)
                         {
-                            r[num % 10] += dezmil;
-                            num /= 10;
+                            num = a;
+
+                            while (num != 0)
+                            {
+                                r[num % 10] += dezmil;
+                                num /= 10;
+                            }
                         }
                     }
-                }
 
-                num = max / dezmil;
-                while (num != 0)
-                {
-                    r[num % 10] += 1;
-                    num /= 10;
-                }
-                r[1] -= x;
-                r[0] = (x - 1) * 300 + 192;
-                printArray(r);
-            });
+                    num = max / dezmil;
+                    while (num != 0)
+                    {
+                        r[num % 10] += 1;
+                        num /= 10;
+                    }
+                    r[1] -= x;
+
+                    if (x < 11)
+                    {
+                        r[0] = (x - 1) * 300 + 192;
+                        r[0]++;
+                    }
+                    else
+                    {
+                        r[0] = (10 - 1) * 300 + 192;
+                    #endregion [bla]
+                        a = 10;
+                        while (a < b)
+                        {
+                            a += 10;
+                            ;
+                            r[0] += 4000;
+                            r[0] += getZeros(a - 10) * 10000;
+                        }
+
+                        r[0] += getZeros(b) + 1;
+                    }
+
+                    var temp2 = r.Take(1);
+                    printArray(temp2);
+                    int gg = temp1.First();
+
+                    Console.WriteLine("   " + (asd = temp2.First() == gg));
+
+
+            }
+  
+
+        end:
             Console.Read();
 
         }
+
+        private static int getZeros(int num)
+        {
+            int x = 0;
+            num /= 10;
+            while (num != 0)
+            {
+                if (num % 10 == 0)
+                {
+                    x++;
+                }
+                num /= 10;
+                
+            }
+
+            return x;
+        }
+
+        static bool So1numero1(int num)
+        {
+            while (num != 0)
+            {
+                if (num % 10 != 0)
+                {
+                    return num == 1;
+                }
+
+                num /= 10;
+            }
+
+            return false;
+        }
+
+
 
         static int[] countDigitsLineRapido(int a, int b)
         {
@@ -80,12 +159,11 @@ namespace ConsoleApplication1
             return c;
         }
 
-        static void printArray(int[] c)
+        static void printArray(IEnumerable<int> c)
         {
-            for (int i = 0, len = c.Length; i < len; i++)
-                Console.Write(c[i] + " ");
+            for (int i = 0, len = c.Count(); i < len; i++)
+                Console.Write(c.ElementAt(i) + " ");
 
-            Console.Write("\n");
         }
 
         static void printTimeSpent(Action action)
@@ -95,6 +173,14 @@ namespace ConsoleApplication1
             stopwatch.Stop();
             GC.Collect();
             Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static bool isBetween(this int i, int fromInclusive, int toInclusive)
+        {
+            return fromInclusive <= i && i <= toInclusive;
         }
     }
 }
