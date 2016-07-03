1617c75c7979dbaba2f140e45ec7ffbf11fbde21
Leandro Fernandes <leandromoh@hotmail.com>
2016-07-02 21:00:35 -0300

    Create array initializer syntax.cs

---
+++ C#/Estudos/array initializer syntax.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+namespace DummyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://msdn.microsoft.com/en-us/library/aa664573(VS.71).aspx
+
+            int[] a1 = new int[2] { 1, 2 };
+
+            int[] a2 = { 1, 2 };
+
+            var a3 = new[] { 1, 2 };
+
+            Console.WriteLine(a1.SequenceEqual(a2) && a2.SequenceEqual(a3));
+
+            Console.Read();
+        }
+    }
+}
