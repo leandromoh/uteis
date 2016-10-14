8c5dd4f0be756648ab4cdf2f8b1b029f6902d2d0
Leandro Fernandes <leandromoh@hotmail.com>
2016-10-13 22:36:10 -0300

    Update NthOrDefault.cs

--- C#/Desafios/linq/NthOrDefault.cs
+++ C#/Desafios/linq/NthOrDefault.cs
@@ -7,48 +7,56 @@ namespace EffectiveLINQ
     {
         public static T NthOrDefault<T>(this IEnumerable<T> source, int nth)
         {
-            return NthIterator(source, nth, null, true, false);
-        }
-
-        public static T Nth<T>(this IEnumerable<T> source, int nth)
-        {
-            return NthIterator(source, nth, null, false, false);
+            return NthIterator(source, nth, true);
         }
 
         public static T NthOrDefault<T>(this IEnumerable<T> source, int nth, Func<T, bool> predicate)
         {
-            return NthIterator(source, nth, predicate, true, true);
+            return NthIterator(source, nth, true, predicate);
+        }
+
+        public static T Nth<T>(this IEnumerable<T> source, int nth)
+        {
+            return NthIterator(source, nth, false);
         }
 
         public static T Nth<T>(this IEnumerable<T> source, int nth, Func<T, bool> predicate)
         {
-            return NthIterator(source, nth, predicate, false, true);
+            return NthIterator(source, nth, false, predicate);
         }
 
-        private static T NthIterator<T>(IEnumerable<T> source, int nth, Func<T, bool> predicate, bool useDefault, bool usePredicate)
+        private static T NthIterator<T>(IEnumerable<T> source, int nth, bool useDefault)
         {
             if (source == null) throw new ArgumentNullException("source");
             if (nth <= 0) throw new ArgumentOutOfRangeException("nth", "nth must be greater than zero.");
-            if (usePredicate && predicate == null) throw new ArgumentNullException("predicate");
 
             int found = 0;
 
-            if (usePredicate)
+            foreach (var item in source)
             {
-                foreach (var item in source)
+                if (++found == nth)
                 {
-                    if (predicate(item))
-                    {
-                        if (++found == nth)
-                        {
-                            return item;
-                        }
-                    }
+                    return item;
                 }
             }
-            else
+
+            if (!useDefault)
+                throw new InvalidOperationException("Sequence does not contains " + nth + " elements");
+
+            return default(T);
+        }
+
+        private static T NthIterator<T>(IEnumerable<T> source, int nth, bool useDefault, Func<T, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+            if (nth <= 0) throw new ArgumentOutOfRangeException("nth", "nth must be greater than zero.");
+
+            int found = 0;
+
+            foreach (var item in source)
             {
-                foreach (var item in source)
+                if (predicate(item))
                 {
                     if (++found == nth)
                     {
@@ -58,12 +66,7 @@ namespace EffectiveLINQ
             }
 
             if (!useDefault)
-            {
-                if (usePredicate)
-                    throw new InvalidOperationException("Sequence does not contains " + nth + " elements that matches the predicate");
-                else
-                    throw new InvalidOperationException("Sequence does not contains " + nth + " elements");
-            }
+                throw new InvalidOperationException("Sequence does not contains " + nth + " elements that matches the predicate");
 
             return default(T);
         }
