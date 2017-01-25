eef4199d73bc44559e347704fb58c6ee3b229634
Leandro Fernandes <leandromoh@hotmail.com>
2017-01-25 13:38:53 -0200

    Update ZipWith.cs

--- C#/Estudos/ZipWith.cs
+++ C#/Estudos/ZipWith.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -25,39 +26,36 @@ namespace ConsoleApplication1
     {
         public static IEnumerable<TResult> Zip<T1, T2, T3, TResult>(this IEnumerable<T1> i1, IEnumerable<T2> i2, IEnumerable<T3> i3, Func<T1, T2, T3, TResult> func)
         {
-            foreach (var item in sele<TResult>(func, i1.Cast<object>(), i2.Cast<object>(), i3.Cast<object>()))
-            {
-                yield return item;
-            }
+            return sele<TResult>(func, i1, i2, i3);
         }
 
         public static IEnumerable<TResult> Zip<T1, T2, T3, T4, TResult>(this IEnumerable<T1> i1, IEnumerable<T2> i2, IEnumerable<T3> i3, IEnumerable<T4> i4, Func<T1, T2, T3, T4, TResult> func)
         {
-            foreach (var item in sele<TResult>(func, i1.Cast<object>(), i2.Cast<object>(), i3.Cast<object>(), i4.Cast<object>()))
-            {
-                yield return item;
-            }
+            return sele<TResult>(func, i1, i2, i3, i4);
+
         }
 
         public static IEnumerable<TResult> Zip<T1, T2, T3, T4, T5, TResult>(this IEnumerable<T1> i1, IEnumerable<T2> i2, IEnumerable<T3> i3, IEnumerable<T4> i4, IEnumerable<T5> i5, Func<T1, T2, T3, T4, T5, TResult> func)
         {
-            foreach (var item in sele<TResult>(func, i1.Cast<object>(), i2.Cast<object>(), i3.Cast<object>(), i4.Cast<object>(), i5.Cast<object>()))
-            {
-                yield return item;
-            }
+            return sele<TResult>(func, i1, i2, i3, i4, i5);
         }
 
-        private static IEnumerable<TResult> sele<TResult>(Delegate func, params IEnumerable<object>[] list)
+        private static IEnumerable<TResult> sele<TResult>(Delegate func, params IEnumerable[] list)
         {
             var e = list.Select(p => p.GetEnumerator()).ToArray();
 
             while (e.All(p => p.MoveNext()))
             {
-                var args = e.Select(p => p.Current).ToArray();
-                yield return (TResult)func.DynamicInvoke(args);
+                yield return (TResult)func.DynamicInvoke(e.Select(p => p.Current).ToArray());
             }
 
-            Array.ForEach(e, p => p.Dispose());
+            Array.ForEach(e, p =>
+            {
+                var d = p as IDisposable;
+
+                if (d != null)
+                    d.Dispose();
+            });
         }
     }
 }
