a55f9d4084eddfc8da47e11df1a3a0d6f3e0aaf5
Leandro Fernandes <leandromoh@hotmail.com>
2017-06-24 23:52:30 -0300

    Update Transpose.cs

--- C#/Desafios/linq/Transpose.cs
+++ C#/Desafios/linq/Transpose.cs
@@ -1,6 +1,6 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
-// Copyright (c) 2016 Leandro F. Vieira (leandromoh). All rights reserved.
+// Copyright (c) 2017 Leandro F. Vieira (leandromoh). All rights reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,87 +23,93 @@ namespace MoreLinq
 
     static partial class MoreEnumerable
     {
-
         /// <summary>
-        /// 
+        /// Return a sequence with the columns in the source swapped into rows.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <returns>
+        /// The transpose version of source.
+        /// </returns>
+        /// <remarks>
+        /// If some of the rows are shorter than the following rows, their elements are skipped.
+        /// This operator uses deferred execution and streams its results.
+        /// </remarks>
+        /// <example>
+        /// <code>
+        /// var result = [[1,2,3],[4,5,6]].Transpose();
+        /// </code>
+        /// The <c>result</c> variable will contain [[1,4],[2,5],[3,6]].
+        /// </example>
         public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> source)
         {
-            return TransposeImpl(source, x => !x.Any());
-        }
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            
+            return _().TakeWhile(x => x.Any());
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="pad"></param>
-        /// <returns></returns>
-        public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> source, T pad)
-        {
-            //shall pass pad to TransposeBla
-            return TransposeImpl(source, x => x.All(y => y.Equals(pad)));
-        }
-
-        private static IEnumerable<IEnumerable<T>> TransposeImpl<T>(this IEnumerable<IEnumerable<T>> source, Func<IEnumerable<T>, bool> predicate)
-        {
-            foreach (var i in TransposeBla(source))
+            IEnumerable<IEnumerable<T>> _()
             {
-                if (predicate(i))
-                    break;
+                var list = new List<IEnumerator<T>>();
+                var values = new List<List<T>>();
+                var empty = new T[0];
+                var count = 0;
 
-                yield return i;
-            }
-        }
+                var buffer = new List<IEnumerable<T>>();
+                var cached = buffer.Concat(EnumerateOnce(source).Pipe(x => buffer.Add(x)));
 
-        private static IEnumerable<IEnumerable<T>> TransposeBla<T>(IEnumerable<IEnumerable<T>> source)
-        {
-            var list = new List<IEnumerator<T>>();
-            var values = new List<List<T>>();
-            var empty = new T[] { }; // should have a list with pad if pad overload
-            var count = 0;
-            var e = source.Select((p, i) =>
-            {
-                if (i < list.Count)
-                    return list[i];
-
-                list.Add(p.GetEnumerator());
-
-                return list[i];
-            });
-
-
-            while (true)
-            {
-                var inner_count = count;
-
-                yield return e.SelectMany((p, j) =>
+                var enumerators = cached.Select((p, i) =>
                 {
-                    if (j >= values.Count)
-                    {
-                        values.Add(new List<T>());
-                    }
+                    if (i < list.Count)
+                        return list[i];
 
-                    var row = values[j];
+                    if (p == null)
+                        throw new InvalidOperationException($"The sequence at index {i} inside source is null.");
 
-                    if (inner_count < row.Count)
-                    {
-                        return new[] { row[inner_count] };
-                    }
+                    list.Add(p.GetEnumerator());
 
-                    while (inner_count >= row.Count && p.MoveNext())
-                    {
-                        row.Add(p.Current);
-                    }
-
-                    return inner_count < row.Count ? new[] { row[inner_count] }
-                                                   : empty;
+                    return list[i];
                 });
 
-                count++;
+                while (true)
+                {
+                    var countClosure = count;
+
+                    yield return enumerators.SelectMany((p, j) =>
+                    {
+                        if (j >= values.Count)
+                            values.Add(new List<T>());
+
+                        var row = values[j];
+
+                        while (countClosure >= row.Count && p != null && p.MoveNext())
+                            row.Add(p.Current);
+
+                        if (countClosure >= row.Count && p != null)
+                        {
+                            list[j].Dispose();
+                            list[j] = null;
+                        }
+
+                        return countClosure < row.Count ? new[] { row[countClosure] } : empty;
+                    });
+
+                    count++;
+                }
+            }
+
+            IEnumerable<U> EnumerateOnce<U>(IEnumerable<U> sequence)
+            {
+                IEnumerator<U> ector = null;
+
+                return f(); IEnumerable<U> f()
+                {
+                    ector = ector ?? sequence.GetEnumerator();
+
+                    while (ector.MoveNext())
+                        yield return ector.Current;
+
+                    ector.Dispose();
+                }
             }
         }
     }
