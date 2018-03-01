676ed8b05353ed6515935b2b9fd250cf93f902e0
Leandro Fernandes <leandromoh@hotmail.com>
2018-02-28 22:30:54 -0300

    Update get & set prop by string.cs

--- C#/Estudos/get & set prop by string.cs
+++ C#/Estudos/get & set prop by string.cs
@@ -3,73 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace teste
 {
-    public class Pessoa
+    public static class NaoSei
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
-    }
-
-    static partial class Program
-    {
-        static void Main(string[] args)
+        public static U SetValue<T, U>(this T obj, string prop, U value)
         {
-            var p = new Pessoa { Nome = "leandro", Idade = 22 };
+            Dictionary<string, Func<object, object, object>> dic = SetProperty(typeof(T));
 
-            Console.WriteLine(p.Prop("Nome"));
-            Console.WriteLine(p.Prop("Idade"));
-
-            p.Prop("Idade", 34);
-
-            Console.WriteLine(p.Idade);
-
-            Console.Read();
-        }
-
-        public static U Prop<T, U>(this T obj, string prop, U value)
-        {
-            Dictionary<string, Func<object, object, object>> func;
-
-            if (!setDicionario.TryGetValue(typeof(T), out func))
+            if (!dic.ContainsKey(prop))
             {
-                setDicionario[typeof(T)] = func = Set(obj);
+                dic = SetField(typeof(T));
             }
 
-            return (U)func[prop](obj, value);
+            return (U)dic[prop](obj, value);
         }
 
-        public static object Prop<T>(this T obj, string prop)
+        public static object GetValue<T>(this T obj, string prop)
         {
-            Dictionary<string, Func<object, object>> func;
+            Dictionary<string, Func<object, object>> dic = GetProperty(typeof(T));
 
-            if (!getDicionario.TryGetValue(typeof(T), out func))
+            if (!dic.ContainsKey(prop))
             {
-                getDicionario[typeof(T)] = func = Get(obj);
+                dic = GetField(typeof(T));
             }
 
-            return func[prop](obj);
+            return dic[prop](obj);
         }
 
-        private static Dictionary<Type, Dictionary<string, Func<object, object>>> getDicionario = new Dictionary<Type, Dictionary<string, Func<object, object>>>();
-        private static Dictionary<Type, Dictionary<string, Func<object, object, object>>> setDicionario = new Dictionary<Type, Dictionary<string, Func<object, object, object>>>();
-
-        private static Dictionary<string, Func<object, object, object>> Set<T>(T obj)
+        private static Dictionary<string, Func<object, object>> Get<T>(Type type, Func<Type, IEnumerable<T>> getList, Func<T, string> getName)
         {
-            var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var props = getList(type);
 
-            var dic = props.ToDictionary(p => p.Name, p =>
+            var dic = props.ToDictionary(getName, p =>
+            {
+                var param = Expression.Parameter(typeof(object), "x");
+                var e = Expression.Convert(param, type);
+
+                var mem = Expression.PropertyOrField(e, getName(p));
+                var conv = Expression.Convert(mem, typeof(object));
+                var exp = Expression.Lambda<Func<object, object>>(conv, param);
+
+                return exp.Compile();
+            });
+
+            return dic;
+        }
+
+        private static Dictionary<string, Func<object, object, object>> Set<T>(Type type, Func<Type, IEnumerable<T>> getList, Func<T, string> getName, Func<T, Type> getType)
+        {
+            var props = getList(type);
+
+            var dic = props.ToDictionary(getName, p =>
             {
                 var param1 = Expression.Parameter(typeof(object), "x");
-                var e1 = Expression.Convert(param1, typeof(T));
+                var e1 = Expression.Convert(param1, type);
 
                 var param2 = Expression.Parameter(typeof(object), "y");
-                var e2 = Expression.Convert(param2, p.PropertyType);
+                var e2 = Expression.Convert(param2, getType(p));
 
-                var mem = Expression.PropertyOrField(e1, p.Name);
+                var mem = Expression.PropertyOrField(e1, getName(p));
 
                 var ass = Expression.Assign(mem, e2);
 
@@ -82,26 +76,38 @@ namespace teste
             return dic;
         }
 
-        private static Dictionary<string, Func<object, object>> Get<T>(T obj)
+        private static BindingFlags flag = BindingFlags.Public | BindingFlags.Instance;
+
+        private static readonly Func<Type, Dictionary<string, Func<object, object, object>>> SetProperty = 
+            Cache((Type t) => Set(t, type => type.GetProperties(flag), p => p.Name, p => p.PropertyType));
+
+        private static readonly Func<Type, Dictionary<string, Func<object, object, object>>> SetField = 
+            Cache((Type t) => Set(t, type => type.GetFields(flag), p => p.Name, p => p.FieldType));
+
+        private static readonly Func<Type, Dictionary<string, Func<object, object>>> GetProperty = 
+            Cache((Type t) => Get(t, type => type.GetProperties(flag), p => p.Name));
+
+        private static readonly Func<Type, Dictionary<string, Func<object, object>>> GetField = 
+            Cache((Type t) => Get(t, type => type.GetFields(flag), p => p.Name));
+
+        private static Func<TSource, TResult> Cache<TSource, TResult>(Func<TSource, TResult> func)
         {
-            var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var cache = new Dictionary<TSource, TResult>();
 
-            var dic = props.ToDictionary(p => p.Name, p =>
+            return (TSource arg) =>
             {
-                var param = Expression.Parameter(typeof(object), "x");
-                var e = Expression.Convert(param, typeof(T));
+                TResult value;
 
-                var mem = Expression.PropertyOrField(e, p.Name);
-                var conv = Expression.Convert(mem, typeof(object));
-                var exp = Expression.Lambda<Func<object, object>>(conv, param);
+                if (!cache.TryGetValue(arg, out value))
+                {
+                    cache[arg] = value = func(arg);
+                }
 
-                return exp.Compile();
-            });
-
-            return dic;
+                return value;
+            };
         }
 
-        private static Dictionary<string, Func<T, object>> GetT<T>(T obj)
+        private static Dictionary<string, Func<T, object>> GetT<T>()
         {
             var props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
@@ -116,7 +122,6 @@ namespace teste
             });
 
             return dic;
-
         }
     }
 }
