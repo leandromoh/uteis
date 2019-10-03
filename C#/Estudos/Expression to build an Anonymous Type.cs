8b16267e1ea290d29c70df3fecc136711e6d5f72
Leandro Fernandes <leandromoh@hotmail.com>
2019-10-03 10:41:18 -0300

    Create  Expression to build an Anonymous Type.cs

---
+++ C#/Estudos/Expression to build an Anonymous Type.cs
@@ -0,0 +1,21 @@
+// https://stackoverflow.com/a/32098234
+
+public static Func<object[], T> AnonymousInstantiator<T>(T example)
+{
+    var ctor = typeof(T).GetConstructors().First();
+    var paramExpr = Expression.Parameter(typeof(object[]));
+    return Expression.Lambda<Func<object[], T>>
+    (
+        Expression.New
+        (
+            ctor,
+            ctor.GetParameters().Select
+            (
+                (x, i) => Expression.Convert
+                (
+                    Expression.ArrayIndex(paramExpr, Expression.Constant(i)),
+                    x.ParameterType
+                )
+            )
+        ), paramExpr).Compile();
+}
